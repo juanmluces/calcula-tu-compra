a23bbc61d69228cfdf7f3fee9903e661692c7f6b
Juan Miguel Luces Rincon <juanmluces@gmail.com>
2023-01-04 15:58:37 +0100

    environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
- // apiUrl: "http://localhost:3000/"
-   apiUrl: "https://new-calcula-tu-compra.herokuapp.com/"
+  apiUrl: "http://localhost:3000/"
+  //apiUrl: "https://new-calcula-tu-compra.herokuapp.com/"
 };
 
 /*
